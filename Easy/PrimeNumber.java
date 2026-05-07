7e74ec26fb3a6f54ebc7ffeda728667c2e2ba1eb
Vivek <vivekmishra7394@gmail.com>
2026-05-07 17:44:02 -0400

    easy folder created.

---
+++ Easy/PrimeNumber.java
@@ -0,0 +1,8 @@
+public class PrimeNumber{
+    public static void main(String[]args){
+        System.out.println("hello");
+        System.out.println("heyaa");
+        System.out.println("i am in a github branch");
+    }
+   
+}
