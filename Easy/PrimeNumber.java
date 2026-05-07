0defa73f97fcc369355f5e30930eeba347e0cd99
vivekmishra7394 <109514162+vivekmishra7394@users.noreply.github.com>
2026-05-07 17:45:21 -0400

    PrimeNumber.java
    updated through github

--- Easy/PrimeNumber.java
+++ Easy/PrimeNumber.java
@@ -3,6 +3,7 @@ public class PrimeNumber{
         System.out.println("hello");
         System.out.println("heyaa");
         System.out.println("i am in a github branch");
+        System.out.println("i am in a github branch updated");
     }
    
 }
