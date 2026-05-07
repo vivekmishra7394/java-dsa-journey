b0f173c73a6186dde0f237d5f883aea4ca7a5d06
Vivek <vivekmishra7394@gmail.com>
2026-05-07 18:37:44 -0400

    Prime number code completed

--- Easy/PrimeNumber.java
+++ Easy/PrimeNumber.java
@@ -1,9 +1,18 @@
 public class PrimeNumber{
     public static void main(String[]args){
-        System.out.println("hello");
-        System.out.println("heyaa");
-        System.out.println("i am in a github branch");
-        System.out.println("i am in a github branch updated");
+        for(int i=2;i<100;i++){
+            boolean isPrime = true;
+            for(int j=2;j<i;j++){
+                if(i%j==0){
+                    isPrime = false;
+                    break;
+                }
+            }
+                if(isPrime){
+                    System.out.print(i+" ");
+                
+            }
+        }
     }
    
 }
