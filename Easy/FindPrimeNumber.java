beafe4c530e17b5851dae2b359166323c888299b
Vivek <vivekmishra7394@gmail.com>
2026-05-08 15:42:10 -0400

    DOne with FindPrimeNumber code.

---
+++ Easy/FindPrimeNumber.java
@@ -0,0 +1,24 @@
+public class FindPrimeNumber {
+
+    public static void main(String[] args) {
+        
+        int n = 83;
+    
+        boolean isPrime = true;
+        for (int i=2;i<n;i++){
+    if(n%i==0){
+        isPrime = false;
+        break;
+    }
+}
+if(isPrime){
+
+    System.out.println("yes");
+
+    }
+    else{
+        System.out.println("no");
+    }
+}
+    }
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # java-dsa-journey
+
+PrimeNumber
