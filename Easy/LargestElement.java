1d37f546f2a1aa0976dc7cb84c02f83c7c3a08fa
Vivek <vivekmishra7394@gmail.com>
2026-05-08 16:12:11 -0400

    LargestELement code done.

---
+++ Easy/LargestElement.java
@@ -0,0 +1,17 @@
+public class LargestElement {
+    public static void main(String[] args) {
+        int arr[] = {3,54,4,56,465,6,8};
+
+        int max = arr[0];
+
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+
+                max = arr[i];
+               
+            }
+        }
+        
+        System.out.println(max);
+    }
+}
